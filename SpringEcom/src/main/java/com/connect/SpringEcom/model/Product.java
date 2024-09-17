df840728933abbbea80b3b0fb04943f78cd7bfec
sabin <hellosabin7@gmail.com>
2024-09-17 11:59:15 +0545

    done

---
+++ SpringEcom/src/main/java/com/connect/SpringEcom/model/Product.java
@@ -0,0 +1,38 @@
+package com.connect.SpringEcom.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Data                   // ---> Lombok ---> Auto creates getter setters
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String description;
+    private String brand;
+    private BigDecimal price;
+    private String category;
+//    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
+    private Date releaseDate;
+    private boolean productAvailable;
+    private int stockQuantity;
+    private String imageName;
+    private String imageType;
+    @Lob    //Large Binary Object
+    private byte[] imageData;
+
+    // Constructor with id only
+    public Product(int id) {
+        this.id = id;
+    }
+}
